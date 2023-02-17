fb0f3fd0bbf72cc04dc7e8627fd37ef195e72635
Lee <ljs0429777@gmail.com>
2023-02-17 14:53:31 +0900

    refactor : 불필요한 lombok 어노테이션 리펙토링

--- src/main/java/com/jeongseok/boardapp/exception/UserException.java
+++ src/main/java/com/jeongseok/boardapp/exception/UserException.java
@@ -2,15 +2,8 @@ package com.jeongseok.boardapp.exception;
 
 import com.jeongseok.boardapp.type.ErrorCode;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter @Setter
 @AllArgsConstructor
-@NoArgsConstructor
-@Builder
 public class UserException extends RuntimeException {
 
 	private ErrorCode errorCode;
