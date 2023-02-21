c507ddfabe7af347477daecc6c6b1333fe69317d
Lee <ljs0429777@gmail.com>
2023-02-21 21:54:24 +0900

    refactor : 불필요한 코드 삭제

--- src/main/java/com/jeongseok/boardapp/controller/CommentController.java
+++ src/main/java/com/jeongseok/boardapp/controller/CommentController.java
@@ -1,19 +1,13 @@
 package com.jeongseok.boardapp.controller;
 
-import com.jeongseok.boardapp.dto.CommentInfo;
 import com.jeongseok.boardapp.service.CommentService;
 import java.security.Principal;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -22,27 +16,12 @@ public class CommentController {
 
 	private final CommentService commentService;
 
-	@GetMapping("/post/{postId}/comment")
-	public String getComments(@PathVariable Long postId, Model model) {
-
-//		List<CommentInfo> comments = commentService.getCommentsByCommentType(postId).stream()
-//			.map(commentDto -> CommentInfo.builder()
-//				.comment(commentDto.getComment())
-//				.updatedAt(commentDto.getUpdatedAt())
-//				.build())
-//			.collect(Collectors.toList());
-//
-//		model.addAttribute("comments", comments);
-
-		return "post/{postId}";
-	}
-
 	@PostMapping("/post/{postId}/comment")
 	public String writeComment(@PathVariable Long postId, @RequestParam String comment, Principal principal) {
 
 		commentService.writeComment(postId, comment, principal.getName());
 
-		return "post/{postId}";
+		return "redirect:/post/" + postId;
 	}
 
 	@PatchMapping("/post/{postId}/comment/{commentId}")
@@ -50,7 +29,7 @@ public class CommentController {
 
 		commentService.updateComment(postId, commentId, comment, principal.getName());
 
-		return "post/{postId}";
+		return "redirect:/post/" + postId;
 	}
 
 	@DeleteMapping("/post/{postId}/comment/{commentId}")
@@ -58,6 +37,6 @@ public class CommentController {
 
 		commentService.deleteComment(postId, commentId, principal.getName());
 
-		return "post/{postId}";
+		return "redirect:/post/" + postId;
 	}
 }
