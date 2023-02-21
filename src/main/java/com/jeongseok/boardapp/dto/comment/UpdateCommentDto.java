8e5683a46dbcec55534b843f9e0efa82ca64a369
Lee <ljs0429777@gmail.com>
2023-02-21 21:55:24 +0900

    feat : 댓글 수정에 필요한 dto 생성

---
+++ src/main/java/com/jeongseok/boardapp/dto/comment/UpdateCommentDto.java
@@ -0,0 +1,19 @@
+package com.jeongseok.boardapp.dto.comment;
+
+import javax.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UpdateCommentDto {
+
+	@NotBlank
+	private String comment;
+}
