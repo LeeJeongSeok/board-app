7413e15b22c987c57e200fc8d68c501c2ed5984d
Lee <ljs0429777@gmail.com>
2023-03-02 17:41:58 +0900

    fix : form 로그인, oauth 로그인 댓글 작성/수정/삭제 오류 수정

--- src/main/java/com/jeongseok/boardapp/controller/CommentController.java
+++ src/main/java/com/jeongseok/boardapp/controller/CommentController.java
@@ -4,8 +4,10 @@ import static com.jeongseok.boardapp.util.ValidationUtil.validationRequestValue;
 
 import com.jeongseok.boardapp.dto.comment.CreateComment;
 import com.jeongseok.boardapp.dto.comment.UpdateComment;
+import com.jeongseok.boardapp.dto.user.CreateUser;
 import com.jeongseok.boardapp.service.CommentService;
 import java.security.Principal;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -23,35 +25,43 @@ public class CommentController {
 	private final CommentService commentService;
 
 	@PostMapping("/post/{postId}/comment")
-	public String writeComment(@PathVariable long postId, @Valid CreateComment.Request request, Errors errors, Principal principal, Model model) {
+	public String writeComment(@PathVariable long postId, @Valid CreateComment.Request request, Errors errors, HttpSession httpSession, Model model) {
 
 		if (errors.hasErrors()) {
 			model.addAttribute(validationRequestValue(errors));
 
 			return "redirect:/post/" + postId;
 		}
-		commentService.writeComment(postId, request, principal.getName());
+
+		CreateUser.Response user = (CreateUser.Response) httpSession.getAttribute("user");
+
+		commentService.writeComment(postId, request, user.getUsername());
 
 		return "redirect:/post/" + postId;
 	}
 
 	@PatchMapping("/post/{postId}/comment/{commentId}")
-	public String updateComment(@PathVariable long postId, @PathVariable long commentId, @Valid UpdateComment.Request request, Errors errors, Principal principal, Model model) {
+	public String updateComment(@PathVariable long postId, @PathVariable long commentId, @Valid UpdateComment.Request request, Errors errors, HttpSession httpSession, Model model) {
 
 		if (errors.hasErrors()) {
 			model.addAttribute(validationRequestValue(errors));
 
 			return "redirect:/post/" + postId;
 		}
-		commentService.updateComment(commentId, request, principal.getName());
+
+		CreateUser.Response user = (CreateUser.Response) httpSession.getAttribute("user");
+
+		commentService.updateComment(commentId, request, user.getUsername());
 
 		return "redirect:/post/" + postId;
 	}
 
 	@DeleteMapping("/post/{postId}/comment/{commentId}")
-	public String deleteComment(@PathVariable long postId, @PathVariable long commentId, Principal principal) {
+	public String deleteComment(@PathVariable long postId, @PathVariable long commentId, HttpSession httpSession) {
 
-		commentService.deleteComment(commentId, principal.getName());
+		CreateUser.Response user = (CreateUser.Response) httpSession.getAttribute("user");
+
+		commentService.deleteComment(commentId, user.getUsername());
 
 		return "redirect:/post/" + postId;
 	}
